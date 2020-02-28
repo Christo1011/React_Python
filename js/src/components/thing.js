1cbcb53f385781506f5fc5dc10c65329475181be
christodoulos <nicolaouchristodoulos58@gmail.com>
2020-02-28 10:14:22 -0500

    experiment1

--- js/src/components/thing.js
+++ js/src/components/thing.js
@@ -1,11 +1,11 @@
 import React from 'react'
-//import imgabout from './H.png';
+//import imgabout from './H.png'
 export default function Thing( props ) {
   return (
       <div className = {props.className |"foo"}>
       <canvas ref= {props.ref |"canvas"} height = {props.height | 300} 
       width = { props.width | 300} />
-      <img src = {props.img |"./H.png"} height = {props.height | 30} width = { props.width | 30}/>
+      <img src = {"H.png"} height = {props.height | 30} width = { props.width | 30}/>
       </div>
       
   );
