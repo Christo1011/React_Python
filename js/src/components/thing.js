5729c29f1d7d2cb36b4751053b7e5ed2161724ad
Steve Spicklemire <steve@spvi.com>
2020-03-06 05:37:22 -0500

    first stab at class version

--- js/src/components/thing.js
+++ js/src/components/thing.js
@@ -1,12 +1,18 @@
 import React from 'react'
-//import imgabout from './H.png'
-export default function Thing( props ) {
-  return (
-      <div className = {props.className |"foo"}>
-      <canvas ref= {props.ref |"canvas"} height = {props.height | 300} 
-      width = { props.width | 300} />
-      <img src = {"H.png"} height = {props.height | 30} width = { props.width | 30}/>
-      </div>
-      
-  );
+
+class Thing extends React.Component {
+
+    
+    render () {
+
+        return (
+            <div className = {this.props.className |"foo"}>
+            <canvas ref="canvas" height = {this.props.height | 300} 
+            width = { this.props.width | 300} />
+            <img src = {"H.png"} height = {this.props.height | 30} width = { this.props.width | 30}/>
+            </div>
+            )
+    }
 }
+
+export default Thing
